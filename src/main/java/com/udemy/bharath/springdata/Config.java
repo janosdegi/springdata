b38ebe3422425dd24d2c6d0d6d6b8b7042c07ebb
Dégi János <johann.degi@gmail.com>
2018-12-11 11:23:14 +0100

    TaskExecutor threadPoolTaskExecutor

---
+++ src/main/java/com/udemy/bharath/springdata/Config.java
@@ -0,0 +1,26 @@
+package com.udemy.bharath.springdata;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+/**
+ * Created by jd on 2018.12.11..
+ */
+@Configuration
+@ComponentScan(basePackages = "com.udemy.*")
+public class Config {
+
+    @Bean
+    public TaskExecutor threadPoolTaskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(4);
+        executor.setMaxPoolSize(4);
+        executor.setThreadNamePrefix("default_task_executor_thread");
+        executor.initialize();
+        return executor;
+    }
+
+}
--- src/test/java/com/udemy/bharath/springdata/SpringdataCustomerTests.java
+++ src/test/java/com/udemy/bharath/springdata/SpringdataCustomerTests.java
@@ -6,18 +6,28 @@ import com.udemy.bharath.springdata.repository.CustomerRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.ConfigFileApplicationContextInitializer;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
+
+import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 @RunWith(SpringRunner.class)
+@ContextConfiguration(classes = Config.class, loader = AnnotationConfigContextLoader.class, initializers = ConfigFileApplicationContextInitializer.class)
 @SpringBootTest
 public class SpringdataCustomerTests {
 
 	@Autowired
 	CustomerRepository customerRepository;
+
+	@Autowired
+	public TaskExecutor taskExecutor;
 
 	@Test
 	public void contextLoads() {
@@ -30,5 +40,22 @@ public class SpringdataCustomerTests {
 		customerRepository.save(customer);
 	}
 
+	@Test
+	public void testThread() {
+		taskExecutor.execute(()-> {
+			try {
+				TimeUnit.SECONDS.sleep(10);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			System.out.println(" _2_ ");
+		});
+		System.out.println(" ");
+		System.out.println(" ");
+		System.out.println(" ");
+		System.out.println(" ");
+		System.out.println(" ");
+		System.out.println(" _1_ ");
+	}
 
 }
